a624d5970f55c9473e197976fe702bb23c0ff028
Someone <75336818+Piscuss123@users.noreply.github.com>
2025-06-06 07:54:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const params = new URLSearchParams({
+    key: processs.env.API_KEY
+});
+
+fetch('https://api.steampowered.com/IGameServersService/GetServerList/v1/?${params}')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json(); // Or response.text() for plain text
+    })
+    .then(data => {
+        console.log(data); // Process the data
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
